e7ec6ee16d89585e86194bf1f612fac536ed6272
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-11-28 11:34:48 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Home = () => {
-    return (
-        <div>
-            <ul>
-                <li> <Link to="/">Home</Link></li>
-                <li> <Link to="/about">About</Link></li>
-            </ul>
-            <h1>Welcome to my website</h1>
-        </div>
-    )
+  return (
+    <div>
+        <h1>Welcome to my website!</h1>
+    </div>
+  )
 }
 
 export default Home
